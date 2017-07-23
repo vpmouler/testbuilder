26c67a2191e49faed8764f154356e6a8a997096d
Seva Mouler <vpmouler@gmail.com>
2017-07-22 18:42:01 -0700

    completed American Express and Diners Club verification

--- detectNetwork.js
+++ detectNetwork.js
@@ -13,6 +13,17 @@ var detectNetwork = function(cardNumber) {
   // The American Express network always starts with a 34 or 37 and is 15 digits long
 
   // Once you've read this, go ahead and try to implement this function, then return to the console.
+  if (cardNumber.length == 15 && cardNumber.slice(0,2) == 34 || cardNumber.slice(0,2) == 37)
+  	return "American Express";
+  if (cardNumber.length == 14 && cardNumber.slice(0,2) == 38 || cardNumber.slice(0,2) == 39)
+  	return "Diner's Club";
 };
 
-
+/*
+if (cardNumber.slice(0,4) == 6011 || cardNumber.slice(0,6) >= 622126 && cardNumber.slice(0,6) <= 622925 || cardNumber.slice(0,2) == '65' || cardNumber.slice(0,3) >= 644 && cardNumber.slice(0,3) <= 649) 
+    return "Discover Card";
+  if (cardNumber.slice(0,1) == 4)
+    return "Visa";
+  if (cardNumber.slice(0,4) >= 3528 && cardNumber.slice(0,4) <= 3589)
+    return "JCB";
+    */
