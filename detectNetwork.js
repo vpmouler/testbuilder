dd9be33e9de5f403f19cc5d082d9f8ecfcdaa1c7
Seva Mouler <vpmouler@gmail.com>
2017-08-17 20:33:31 -0700

    returning to this after initially speaking with Arturo in SSP about this. Having issue with not being able to proceed with error of tests failing, while all tests on Chrome are showing as passed. Worked with Arturo to see if there are issues with code - there were none. Used his code to see if there are bugs, seemed to work. Pushing this up now and will try to redo from scratch, first deleting the repo.

--- detectNetwork.js
+++ detectNetwork.js
@@ -1,52 +1,94 @@
-// Given a credit card number, this function should return a string with the 
-// name of a network, like 'MasterCard' or 'American Express'
-// Example: detectNetwork('343456789012345') should return 'American Express'
-
-// How can you tell one card network from another? Easy! 
-// There are two indicators:
-//   1. The first few numbers (called the prefix)
-//   2. The number of digits in the number (called the length)
-
 var detectNetwork = function(cardNumber) {
-  // Note: `cardNumber` will always be a string
-  // The Diner's Club network always starts with a 38 or 39 and is 14 digits long
-  // The American Express network always starts with a 34 or 37 and is 15 digits long
 
-  // Once you've read this, go ahead and try to implement this function, then return to the console.
-  if (cardNumber.length == 15 && cardNumber.slice(0,2) == 34 || cardNumber.slice(0,2) == 37)
-  	return "American Express";
-  if (cardNumber.length == 14 && cardNumber.slice(0,2) == 38 || cardNumber.slice(0,2) == 39)
-  	return "Diner's Club";
-  if (cardNumber.length == 16 && cardNumber.slice(0,2) >= 51 && cardNumber.slice(0,2) <= 55)
-    return "MasterCard";
-  if (cardNumber.slice(0,1) == 4 && cardNumber.length == 13 || cardNumber.length == 16 || cardNumber.length == 19)
-    return "Visa"; // why is this returning visa when on top of mastercard?
-  if (cardNumber.length === 16 || cardNumber.length === 19 && cardcardNumber.slice(0,4) == 6011 || cardNumber.slice(0,6) >= 622126 && cardNumber.slice(0,6) <= 622925 || cardNumber.slice(0,2) == 65 || cardNumber.slice(0,3) >= 644 && cardNumber.slice(0,3) <= 649) 
-    return "Discover Card";
-  if (cardNumber.length >= 12 && cardNumber.length <= 19 && cardNumber.slice(0,4) == '5018' || cardNumber.slice(0,4) == 5020 || cardNumber.slice(0,4) == 5038 || cardNumber.slice(0,4) == 6304)
-    return "Maestro";
+  var cardType;
+  var prefixOne   = cardNumber[0];
+  var prefixTwo   = cardNumber.slice(0, 2);
+  var prefixThree = cardNumber.slice(0, 3);
+  var prefixFour  = cardNumber.slice(0, 4);
+  var prefixSix   = cardNumber.slice(0, 6);
+  var len         = cardNumber.length;
+
+  // Diner's Club
+
+  if(len === 14 && prefixTwo === "38" || prefixTwo === "39") cardType = "Diner's Club";
+
+  // American Express
+
+  if(len === 15 && prefixTwo === "34" || prefixTwo === "37") cardType = "American Express";
+
+  // Visa
+
+  if(prefixOne === "4") {
+    switch(len) {
+      case 13:
+      case 16:
+      case 19:
+        cardType = "Visa";
+    }
+  }
+
+  // MasterCard
+
+  if(len === 16) {
+    switch(prefixTwo) {
+      case "51":
+      case "52":
+      case "53":
+      case "54":
+      case "55":
+        cardType = "MasterCard";
+    }
+  }
+
+  // Discover
+
+  if(len === 16 || len === 19) {
+    if(prefixFour === "6011" || prefixTwo === "65") cardType = "Discover";
+    switch(prefixThree) {
+      case "644":
+      case "645":
+      case "646":
+      case "647":
+      case "648":
+      case "649":
+        cardType = "Discover";
+    }
+  }
+
+  // Maestro
+
+  if(prefixFour === "5018" || prefixFour === "5020" ||
+     prefixFour === "5038" || prefixFour === "6304") {
+       if(len >= 12 && len <= 19) cardType = "Maestro";
+  }
+
+  // China UnionPay
+
+  if(len >= 16 && len <= 19) {
+    if(parseInt(prefixSix) >= 622126 && parseInt(prefixSix) <= 622925) {
+      cardType = "China UnionPay";
+    }
+    if(parseInt(prefixFour) >= 6282 && parseInt(prefixFour) <= 6288) {
+      cardType = "China UnionPay";
+    }
+    if(parseInt(prefixThree) >= 624 && parseInt(prefixThree) <= 626) {
+      cardType = "China UnionPay";
+    }
+  }
+
+  // Switch
+
+  if(len === 16 || len === 18 || len === 19) {
+    switch (prefixFour) {
+      case "4903":
+      case "4905":
+      case "4911":
+      case "4936":
+        cardType = "Switch";
+    }
+    if(prefixSix === "564182" || prefixSix === "633110") cardType = "Switch";
+    if(prefixFour === "6333" || prefixFour === "6759") cardType = "Switch";
+  }
+
+  return cardType;
 };
-
-var visa = "4123456789012345678";
-
-console.log(detectNetwork(visa))
-
-/*
-var Maestro = '501819293918204'
-console.log(Maestro.slice(0,4) === 5018)
-console.log(detectNetwork(Maestro))
-
-
-
-  // if (cardNumber.slice(0,4) >= 3528 && cardNumber.slice(0,4) <= 3589)
-    // return "JCB";
-
-
-var MasterCard = "5512345678901234";
-var visa = "4123456789012345678";
-
-console.log(detectNetwork(visa));
-
-console.log(visa[0])
-
-*/
